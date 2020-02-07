8a394cc8319775d320527cc077f00be0a226e6d3
Nego Thales <thale.serra97@gmail.com>
2020-02-07 09:21:52 -0300

    implementacao firestore

--- app/src/main/java/com/thales/poc_tcc/MainActivity.java
+++ app/src/main/java/com/thales/poc_tcc/MainActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
 
+
+    private static final String TAG_MAIN = "MainActivity";
     private FirebaseAuth mAuth;
 
     @Override
@@ -30,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("usuarios").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG_MAIN, document.getId() + " => " + document.getData());
+                    }
+                } else {
+                    Log.w(TAG_MAIN, "Error getting documents.", task.getException());
+                }
+            }
+        });
 
     }
 }
